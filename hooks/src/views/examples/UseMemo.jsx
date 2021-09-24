ec011d73989d3cb8457a27ee7999ed53a3584bea
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-09-23 21:12:45 -0300

    useMemo

--- hooks/src/views/examples/UseMemo.jsx
+++ hooks/src/views/examples/UseMemo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 function sum(a, b){
     const future = Date.now() + 2000
